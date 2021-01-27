8ce58d50a35dc6fb6facf6116e5e25371b9c0b34
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-01-27 07:18:49 +0300

    Day 9 level 2

--- 09_Day_Higher_order_functions/09_day_starter/scripts/level2.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/level2.js
@@ -30,3 +30,50 @@ console.log('totalPrice only by reduce:>> ', totalPrice);
 // 3. categorize countries
 import { countries } from '../data/countries_data.js'
 console.log('counries :>> ', countries[0]);
+
+function categorizeCountries(pattern) {
+   let selectedCountries = countries.filter((el) => el.name.includes(pattern));
+   let countriesName = selectedCountries.map(el => el.name);
+   return countriesName
+}
+
+console.log('categorizeCountries(\'land\') :>> ', categorizeCountries('land'));
+console.log('categorizeCountries(\'ia\') :>> ', categorizeCountries('ia'));
+
+// 4. first letter
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function firstLetter(lett) {
+   let filterOut = countries.filter(el => el.name.toLowerCase().startsWith(lett))
+   return filterOut.length
+}
+
+let letObj = [];
+[...alphabet].forEach(lett=> letObj.push({letter: lett.toUpperCase(), number: firstLetter(lett)}));
+console.log('letObj :>> ', letObj);
+
+// 5-6. first and ten countries
+function getFirstTenCountries() {
+   let tenNames = [];
+   countries.slice(0, 10).forEach(el => tenNames.push(el.name));
+   return tenNames
+}
+
+function getLastTenCountries() {
+   let tenNames = [];
+   countries.slice(-10).forEach(el => tenNames.push(el.name));
+   return tenNames
+}
+
+console.log('getFirstTenCountries() :>> ', getFirstTenCountries());
+console.log('getLastTenCountries() :>> ', getLastTenCountries());
+
+// 7. find letter used many times as initial
+let sortedLetObj = [...letObj];
+sortedLetObj.sort(function (a, b) {
+   if (a.number < b.number) return 1
+   if (a.number > b.number) return -1
+   return 0});
+
+console.log('sortedLetObj :>> ', sortedLetObj);
+console.log('sortedLetObj[0] :>> ', sortedLetObj[0]);
