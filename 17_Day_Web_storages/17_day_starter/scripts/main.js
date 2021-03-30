24151d13f4460337e69120722c284eca35cb6e22
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-03-30 13:41:39 +0300

    day 17 level 3

--- 17_Day_Web_storages/17_day_starter/scripts/main.js
+++ 17_Day_Web_storages/17_day_starter/scripts/main.js
@@ -3,8 +3,6 @@
 // Store you first name, last name, age, country, city in your browser localStorage.
 // Exercises: Level 2
 // Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
-// Exercises: Level 3
-// Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
 
 const firstName = 'Andrey';
 const lastName = 'Kurdyubov';
@@ -68,3 +66,77 @@ const student1 = new Student('Andrey',
                              true);
 
 localStorage.setItem('student1', JSON.stringify(student1.personInfo()));
+
+// Exercises: Level 3
+// Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+function personAccount(firstName, lastName, incomes = new Set(), expenses = new Set()) {
+   this.firstName = firstName;
+   this.lastName = lastName;
+   this.incomes = incomes;
+   this.expenses = expenses;
+   
+   this.totalIncome = function() {
+      let summ = 0;
+      [...this.incomes].forEach(x => {summ += x[0]});
+      return summ; 
+   }
+   this.totalExpense = function() {
+      let summ = 0;
+      [...this.expenses].forEach(x => {summ += x[0]});
+      return summ; 
+   }
+   this.addIncome = function(newIncome, descr) {
+      this.incomes.add([newIncome, descr]);
+   }
+   this.addExpense = function(newExpense, descr) {
+      this.expenses.add([newExpense, descr]);
+   }
+   this.accountBalance = function() {
+      return this.totalIncome() - this.totalExpense();
+   }
+   this.accountInfo = function() {
+      console.log(`${this.firstName} ${this.lastName}`);
+      [...this.incomes].forEach(x => {
+         console.log(`income: ${x[1]} ${x[0]}`);
+      });
+      [...this.expenses].forEach(x => {
+         console.log(`expence: ${x[1]} ${x[0]}`);
+      });
+      console.log(`total income: ${this.totalIncome()}`);
+      console.log(`total expense: ${this.totalExpense()}`);
+      console.log(`balance: ${this.accountBalance()}`);
+   }
+}
+
+const me = new personAccount('A', 'B');
+console.log('me.expenses :>> ', me.expenses);
+me.addExpense(10, 'lalala');
+me.addExpense(20, 'blala');
+me.addIncome(25, 'y55');
+me.addIncome(27, 'y77');
+console.log('me.totalExpense() :>> ', me.totalExpense());
+console.log('me.totalIncome() :>> ', me.totalIncome());
+console.log('me.accountBalance() :>> ', me.accountBalance());
+me.accountInfo();
+
+localStorage.setItem('me', JSON.stringify(me));
+console.log('JSON.stringify(me, undefined, 2) :>> ', JSON.stringify(me, undefined, 2));
+// Sets do not get stingified
+console.log('me :>> ', me);
+
+// const arr = [...me.econsole.log('me.accountBalance() :>> ', me.accountBalance());xpenses];
+// console.log('arr :>> ', arr);
+// console.log('arr.reduce((a, b) => a[0] + b[0], 0) :>> ', arr.reduce((a, b) => a[0] + b[0], 0));
+
+// const arr2 = [[1, '3g'], [2, '3l'], [4, '4g']];
+// console.log('arr2.reduce((p, c) => p[0] + c[0], 0) :>> ', arr2.reduce((p, c) => p[0] + c[0], 0));
+
+// const arr3 = [1, 2, 4];
+// console.log('arr3.reduce((p, c) => p + c, 0) :>> ', arr3.reduce((p, c) => p + c, 0));
+
+const mySet = new Set([1, '3', 'sdf', false])
+console.log('mySet :>> ', mySet);
+console.log('JSON.stringify(mySet, undefined, 4) :>> ', JSON.stringify(mySet, undefined, 4));
+console.log('[...mySet.keys()] :>> ', [...mySet.keys()]);
+console.log('[...mySet.values()] :>> ', [...mySet.values()]);
