9d852c177113695e489a97434590d273f3c3edf6
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-04-22 09:04:58 +0300

    day 18 ex 2

--- 18_Day_Promises/18_day_starter/scripts/main.js
+++ 18_Day_Promises/18_day_starter/scripts/main.js
@@ -23,3 +23,42 @@ fetch(countriesAPI)
    })
    .catch(error => console.log(error));
 
+const fetchData = async function(url) {
+   try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log('data[0] :>> ', data[0]);
+   }
+   catch (err) {
+      console.log('err :>> ', err);
+   }
+}
+
+fetchData(countriesAPI+'d');
+
+const fetchCats = async (url, names) => {
+   try {
+      let response = await fetch(url);
+      let data = await response.json();
+      data.forEach(element => {
+         names.push(element.name)
+      }
+      // console.log('data :>> ', data); 
+      )}
+   catch (err) {
+      console.log('err :>> ', err);
+   }
+}
+
+let catNames =[];
+fetchCats(catsAPI, catNames);
+console.log('catNames :>> ', catNames);
+
+let catNames2 = [];
+fetch(catsAPI)
+   .then(response => response.json())
+   .then(data => {
+      data.forEach(el => catNames2.push(el.name))
+      console.log('catNames2 :>> ', catNames2);
+   })
+   .catch(err => console.log('err :>> ', err));
