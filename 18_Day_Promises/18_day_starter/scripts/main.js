5f3fce9b830e4afaf4571009286a11cdf74f5084
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-04-23 13:49:49 +0300

    level3 day 18 finished

--- 18_Day_Promises/18_day_starter/scripts/main.js
+++ 18_Day_Promises/18_day_starter/scripts/main.js
@@ -36,57 +36,44 @@ const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 
 // fetchData(countriesAPI+'d');
 
-const fetchCats = async (url) => {
-   try {
-      let response = await fetch(url);
-      let data = await response.json();
-      // data.forEach(element => {
-      //    names.push(element.name);
-      //    weights.push(element.weight);
-      // });
-      // console.log('data :>> ', data[0]); 
-      let catNames = [];
-      let catWeights = [];
-      data.forEach(el=> {
-         catNames.push(el.name);
-         catWeights.push(el.weight);
-      })
-      // console.log('data :>> ', data);
-      // console.log('catNames, catWeights :>> ', catNames, catWeights);
-      return [catNames, catWeights]
-   }
-   catch (err) {
-      console.log('err :>> ', err);
-   }
-}
+// const fetchCats = async (url) => {
+//    try {
+//       let response = await fetch(url);
+//       let data = await response.json();
+//       // data.forEach(element => {
+//       //    names.push(element.name);
+//       //    weights.push(element.weight);
+//       // });
+//       // console.log('data :>> ', data[0]); 
+//       let catNames = [];
+//       let catWeights = [];
+//       data.forEach(el=> {
+//          catNames.push(el.name);
+//          catWeights.push(el.weight);
+//       })
+//       // console.log('data :>> ', data);
+//       // console.log('catNames, catWeights :>> ', catNames, catWeights);
+//       return [catNames, catWeights]
+//    }
+//    catch (err) {
+//       console.log('err :>> ', err);
+//    }
+// }
 
-// let avWght = 0;
-let cats = fetchCats(catsAPI);
-cats.then(([nam, weight]) => {
-   // console.log('result :>> ', result);
-   console.log('nam, weight :>> ', nam, weight);
-   let patt = /\d+/g;
-   let avWt = 0;
-   weight.forEach(el => {
-      let matc = el.metric.match(patt);
-      avWt += (Number(matc[0]) + Number(matc[1])) / 2;
-   });
-   avWt /= weight.length;
-   console.log('avWt :>> ', avWt.toPrecision(2));
-})
-
-// console.log('catNames :>> ', catNames);
-// console.log('catWeights :>> ', catWeights);
-// console.log('catWeights.length :>> ', catWeights.length);
-// catWeights.forEach(el => {
-//       console.log('el :>> ', el);
-      // let pattern = /\d+/g;
-      // let matches = el['metric'].match(pattern);
-      // console.log('matches :>> ', matches);
-      // avWght += matches[0];
-//    })
-// console.log('metric average cat weight :>> ', avWght);
-
+// // let avWght = 0;
+// let cats = fetchCats(catsAPI);
+// cats.then(([nam, weight]) => {
+//    // console.log('result :>> ', result);
+//    console.log('nam, weight :>> ', nam, weight);
+//    let patt = /\d+/g;
+//    let avWt = 0;
+//    weight.forEach(el => {
+//       let matc = el.metric.match(patt);
+//       avWt += (Number(matc[0]) + Number(matc[1])) / 2;
+//    });
+//    avWt /= weight.length;
+//    console.log('avWt :>> ', avWt.toPrecision(2));
+// })
 
 // let catNames2 = [];
 // fetch(catsAPI)
@@ -96,3 +83,26 @@ cats.then(([nam, weight]) => {
 //       console.log('catNames2 :>> ', catNames2);
 //    })
 //    .catch(err => console.log('err :>> ', err));
+
+// Read the countries api and find out the 10 largest countries
+// Read the countries api and count total number of languages in the world used as officials.
+
+let topCountries = [];
+let languages = new Set();
+fetch(countriesAPI)
+   .then(response => response.json())
+   .then(data => {
+      console.log('data[0] :>> ', data[0]);
+      data.forEach(el => {
+         topCountries.push({name: el.name, population: el.population})
+         el.languages.forEach(lang => {
+            languages.add(lang.name)
+         })
+      });
+      topCountries.sort((a, b) => b.population - a.population);
+      console.log('topCountries :>> ', topCountries.slice(0, 10));
+      console.log('languages :>> ', languages, languages.size);
+   })
+   .catch(err => {
+      console.log('err :>> ', err);
+   });
