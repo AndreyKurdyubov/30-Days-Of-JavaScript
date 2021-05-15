263fbf89cedef4736d535f2a151141159fcbc1f7
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-05-15 13:50:40 +0300

    day 22 ex 2

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
@@ -1,1 +1,15 @@
 console.log(countries)
+
+const h4 = document.getElementById('total-countries');
+h4.textContent = `Total number of countries: ${countries.length}`;
+
+const countrWrapper = document.getElementsByClassName('countries-wrapper')[0];
+const countrCont = document.getElementsByClassName('countries-container')[0];
+
+countrWrapper.textContent = countries[0];
+
+for (let i = 1; i < countries.length; i++) {
+   const cloneWrap = countrWrapper.cloneNode(false);
+   const newNode = countrCont.appendChild(cloneWrap);
+   newNode.textContent = countries[i];
+}
