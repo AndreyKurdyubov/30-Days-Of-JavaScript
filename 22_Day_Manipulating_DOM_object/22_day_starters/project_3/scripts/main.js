bc711f40301d9554c1233541a62603ad57cb2c1d
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-05-15 16:23:55 +0300

    day 22 ex 3 header

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_3/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_3/scripts/main.js
@@ -1,1 +1,70 @@
 console.log(asabenehChallenges2020)
+const now = new Date();
+
+const wrapper = document.getElementsByClassName('wrapper')[0];
+const header = document.createElement('div');
+
+const h1 = document.createElement('h1');
+const h2 = document.createElement('h2');
+const h3 = document.createElement('h3');
+const spanYear = document.createElement('span');
+
+spanYear.id = 'year';
+spanYear.textContent = '2020';
+
+h1.textContent = 'Asabeneh Yetayeh challenges in ';
+h1.appendChild(spanYear);
+
+h3.innerHTML = '<a href="https://github.com/Asabeneh/30-Days-Of-JavaScript">30DaysOfJavaScript Challenge</a>';
+
+h2.innerHTML = `${time()}`;
+
+wrapper.appendChild(h1);
+wrapper.appendChild(h3);
+wrapper.appendChild(h2);
+
+let changeColor = setInterval(() => {
+                     h2.style.backgroundColor = newColor();
+                     spanYear.style.color = newColor();
+                  }, 1000);
+
+function time() {
+   const mths = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+];
+   const month = mths[now.getMonth()];
+   const day = now.getDate();
+   const year = now.getFullYear();
+   const hour = now.getHours();
+   const minute = now.getMinutes();
+   const second = now.getSeconds();
+
+   function hms() {
+      let hours = `0${hour}`;
+      let mins = `0${minute}`;
+      let secs = `0${second}`;
+      hours = hours.slice(hours.length - 2);
+      mins = mins.slice(mins.length - 2);
+      secs = secs.slice(secs.length - 2);
+      return `${hours}:${mins}:${secs}`
+   }
+
+   return `${month} ${day}, ${year} ${hms()}`;
+}
+
+function newColor() {
+   const rnd = () => 128 + Math.floor(Math.random() * 128)
+   return `rgb(${rnd()}, ${rnd()}, ${rnd()})`;
+}
+
