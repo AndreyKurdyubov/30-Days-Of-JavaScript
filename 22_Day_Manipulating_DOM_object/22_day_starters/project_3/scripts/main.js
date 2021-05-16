db9f042f92cedebaee03120d4e50d0cd508cf7ff
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2021-05-16 07:52:16 +0300

    day 22 ex 3 challenges part

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_3/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_3/scripts/main.js
@@ -23,12 +23,70 @@ wrapper.appendChild(h1);
 wrapper.appendChild(h3);
 wrapper.appendChild(h2);
 
+// const python = document.createElement('div');
+// const js = document.createElement('div');
+// const web = document.createElement('div');
+// const react = document.createElement('div');
+// const reactNative = document.createElement('div');
+// const fullstack = document.createElement('div');
+// const ML = document.createElement('div');
+
+// python.classList.add('courses', 'done');
+// js.classList.add('courses', 'ongoing');
+// web.classList.add('courses', 'coming');
+// react.classList.add('courses', 'coming');
+// reactNative.classList.add('courses', 'coming');
+// fullstack.classList.add('courses', 'coming');
+// ML.classList.add('courses', 'coming');
+
+let challenges = asabenehChallenges2020['challenges'];
+
+challenges.forEach(challenge => {
+   const newDiv = document.createElement('div');
+   newDiv.classList.add('courses', challenge.status.toLowerCase())
+
+   const item1 = document.createElement('span');
+   if (challenge.githubUrl) {
+      item1.innerHTML = `<a href="${challenge.githubUrl}">${challenge.name}</a>`
+   }
+   else {
+      item1.textContent = challenge.name;
+   }
+
+   const item2 = document.createElement('details');
+   const summary = document.createElement('summary');
+   const ul = document.createElement('ul');
+
+   ul.style.listStyleType = 'none';
+   ul.style.padding = '0';
+   summary.textContent = challenge.name.match(/(?<=30 Days Of )[\w &]*/gi);
+
+   challenge.topics.forEach(el => {
+      const li = document.createElement('li');
+      li.textContent = el;
+      ul.appendChild(li);
+   });
+
+   item2.appendChild(summary);
+   item2.appendChild(ul);
+
+   const item3 = document.createElement('span');
+   item3.textContent = challenge.status;
+
+   newDiv.appendChild(item1);
+   newDiv.appendChild(item2);
+   newDiv.appendChild(item3);
+   wrapper.appendChild(newDiv);
+});
+
+const title = document.createElement('div');
+
 let changeColor = setInterval(() => {
-                     const now = new Date();
-                     h2.innerHTML = `${time(now)}`;
-                     h2.style.backgroundColor = newColor();
-                     spanYear.style.color = newColor();
-                  }, 1000);
+   const now = new Date();
+   h2.innerHTML = `${time(now)}`;
+   h2.style.backgroundColor = newColor();
+   spanYear.style.color = newColor();
+}, 1000);
 
 function time(now) {
    const mths = [
@@ -44,7 +102,7 @@ function time(now) {
       'October',
       'November',
       'December'
-];
+   ];
    const month = mths[now.getMonth()];
    const day = now.getDate();
    const year = now.getFullYear();
